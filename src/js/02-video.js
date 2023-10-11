d332f5490ba74b6af046c7f12d68a3c15d521658
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-10-11 20:38:03 +0300

    corrected

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,13 @@ import throttle from 'lodash/throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+function saveCurrentTime(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}
+const storedCurrentTime = parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
+player.setCurrentTime(storedCurrentTime);
 player.on('timeupdate', throttle(function (data) {
-    player.setCurrentTime().then(function (seconds) {
-         const currentTime = data.seconds;
-         localStorage.setItem('videoplayer-current-time', currentTime.toString())
-  })
-    }, 1000)); 
-
-
+  const currentTime = data.seconds;
+  saveCurrentTime(currentTime);
+}, 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,30 +10,30 @@ function saveFormState() {
   localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
 
-emailInput.addEventListener("input", saveFormState);
-messageTextarea.addEventListener("input", saveFormState);
-
-const savedFormData = localStorage.getItem("feedback-form-state");
-if (savedFormData) {
-  const parsedFormData = JSON.parse(savedFormData);
-  emailInput.value = parsedFormData.email;
-  messageTextarea.value = parsedFormData.message;
+function loadFormState() {
+  const savedFormData = localStorage.getItem("feedback-form-state");
+  if (savedFormData) {
+    const parsedFormData = JSON.parse(savedFormData);
+    emailInput.value = parsedFormData.email;
+    messageTextarea.value = parsedFormData.message;
+  }
 }
 
-form.addEventListener('submit',  throttle(handlerSubmit, 500));
 function handlerSubmit(event) {
-    event.preventDefault();
-    const formData = {
-        email: emailInput.value,
-        message: messageTextarea.value,
-    }
-    console.log(formData);
-    emailInput.value = '';
-    messageTextarea.value = '';
-    localStorage.removeItem("feedback-form-state");
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  console.log(formData);
+  emailInput.value = '';
+  messageTextarea.value = '';
+  localStorage.removeItem("feedback-form-state");
 }
 
-
+form.addEventListener("input", throttle(saveFormState, 500)); 
+form.addEventListener("submit", handlerSubmit); 
+loadFormState();  
 
 
 
